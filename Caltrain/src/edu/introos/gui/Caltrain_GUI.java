877341164e33f2fa7fe2e09921e7522c5639ba3f
Allendale Nato <natoallendale@gmail.com>
2017-03-26 21:45:37 +0800

    Add MiGLayout

---
+++ Caltrain/src/edu/introos/gui/Caltrain_GUI.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.introos.gui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Allendale
+ */
+
+public class Caltrain_GUI {
+    
+    static JFrame mainFrame;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+        public void run() {
+            
+        }
+    });
+    }
+    
+}
